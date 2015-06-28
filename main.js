c7c6a804e34f1df4d1e1dbcb896fbc20c1bd38f6
Kalmani <kolosowski.florent@gmail.com>
2015-06-28 11:48:58 +0200

    proper way to init/reset project

--- main.js
+++ main.js
@@ -1,28 +1,103 @@
 #! /usr/bin/env node
 
 require('mootools');
+var fs = require('fs'),
+    path = require('path'),
+    prompt = require('prompt');
 
 var Koios = new Class({
 
+  Binds : [
+    'clear_dir'
+  ],
+
+  valid_promp_responses : [
+    'y', 'Y', 'yes', 'Yes', 'YES', 'ofc'
+  ],
+
+  CONFIRM_DELETE : "You are about to erease all your projects' files, do you want to continue ? [Y/n]",
+
   initialize : function() {
-    var cli_args = process.argv.splice(2);
+    var self = this;
 
-    this.cmd = cli_args[0];
-    this.arg = cli_args[1];
+    self.cli_args = process.argv.splice(2);
 
-    if(typeof(this[this.cmd]) == "function")
-      return this[this.cmd]();
+    self.cmd = self.cli_args[0];
+    self.arg = self.cli_args[1];
 
-    return this.no_cmd();    
+    if(typeof(self[self.cmd]) == "function")
+      return self[self.cmd]();
+
+    return self.no_cmd();
   },
 
   project : function() {
-    console.log('THIS IS PROJECT !');
+    var self = this,
+        sub_cmd = self.arg || 'init',
+        architecture = JSON.parse(fs.readFileSync(__dirname + '/architecture.json'));
+
+    switch (sub_cmd) {
+      case 'init' :
+      default :
+        console.info('Init project');
+        self.dive_arch(architecture, './');
+        break;
+      case 'reset' :
+        self.ask(self.CONFIRM_DELETE, function(err, response) {
+          if (self.valid_promp_responses.indexOf(response[self.CONFIRM_DELETE]) !== -1) {
+            var first_directories = Object.keys(architecture);
+            Array.each(first_directories, function(path) {
+              self.clear_dir(path)
+            });
+            self.dive_arch(architecture, './');
+            console.log('Project has been reset');
+          } else {
+            console.log('do nothing');
+          }
+        });
+    }
+  },
+
+  clear_dir : function(path) {
+    var self = this;
+    if(fs.existsSync(path)) {
+      Object.each(fs.readdirSync(path), function(file) {
+        var curPath = path + "/" + file;
+        if(fs.lstatSync(curPath).isDirectory()) {
+          self.clear_dir(curPath);
+        } else {
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    }
+  },
+
+  dive_arch : function(architecture, parent) {
+    var self = this;
+    Object.each(architecture, function(sub, folder) {
+      self.mkdirSync(parent + folder);
+      tmp_parent = parent + folder + '/';
+      self.dive_arch(sub, tmp_parent);
+    });
   },
 
   no_cmd : function() {
     console.error('this cmd doesnt exist');
-  }
-});
+  },
 
+  mkdirSync : function (path) {
+    try {
+      fs.mkdirSync(path);
+    } catch(e) {
+      if ( e.code != 'EEXIST' ) throw e;
+    }
+  },
+
+  ask : function(q, callback) {
+    prompt.start();
+    prompt.get([q], callback);
+  }
+
+});
 var koios = new Koios();
